53f0b307841a17e551f691e8fe2c1d60145dd686
Venom <you@example.com>
2025-05-16 16:49:12 +0530

    feat: SignUp and login page ui completed

--- Frontent/src/App.jsx
+++ Frontent/src/App.jsx
@@ -1,14 +1,47 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import SignUpPage from "./pages/Register";
+import HomePage from "./pages/Home";
 
 const App = () => {
-    return (
-    <div className="flex flex-col items-center justify-start">
-      <Routes>
-        <Route path="/login" element={<LoginPage/>}> </Route>
-      </Routes>
-    </div>
-    )
+	let isAuthenticated = null;
+
+	return (
+		<div className="flex flex-col items-center justify-start">
+			<Routes>
+				<Route
+					path="/"
+					element={
+						isAuthenticated ? (
+							<HomePage />
+						) : (
+							<Navigate to={"/login"} />
+						)
+					}
+				/>
+
+				<Route
+					path="/register"
+					element={
+						!isAuthenticated ? (
+							<SignUpPage />
+						) : (
+							<Navigate to={"/"} />
+						)
+					}
+				></Route>
+				<Route
+					path="/login"
+					element={
+						!isAuthenticated ? <LoginPage /> : <Navigate to={"/"} />
+					}
+				>
+					{" "}
+				</Route>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
